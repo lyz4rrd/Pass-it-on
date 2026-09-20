0b92fac68fca41f804b0eb0d05b22bfd8f11e7bc
lyz4rrd <aaboop25@gmail.com>
2026-09-19 23:33:57 -0400

    Restore assets and set up first level

--- player.gd
+++ player.gd
@@ -4,6 +4,8 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+func _ready() -> void:
+	print("Player scene started")
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
